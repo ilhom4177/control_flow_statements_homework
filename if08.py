da694f0805457f9dadb4e38c7769db805bb977e3
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-22 23:02:08 +0500

    if08.py

--- if08.py
+++ if08.py
@@ -11,13 +11,18 @@ def main(a):
     Returns:
         string: the message to print
     """ 
-    if a//10!=0 and a%2!=0:
-        return 'two-digit odd number'
-    if a//10!=0 and a%2==0:
-        return 'two-digit even number'
-    if a//100!=0 and a%2!=0:
-        return 'three-digit odd number'
-    if a//100!=0 and a%2==0:
-        return 'three-digit even number'
+    if  a // 10 != 0  and  a % 2 != 0 :
+        if  9<a<100 and a%2!=0:
+            return 'two-digit odd number'
+    if  a // 10 != 0  and  a % 2 == 0 :
+        if  9<a<100 and a%2==0:
+            return 'two-digit even number'
+    if  a // 100 != 0  and  a % 2 != 0 :
+        if 99<a<1000 and a%2!=0:
+            return 'three-digit odd number'
+    if  a // 100 != 0  and  a % 2 == 0 :
+        if 99<a<1000 and a%2==0:
+            return 'three-digit even number'
 print(main(24))
+print(main(100))
     
